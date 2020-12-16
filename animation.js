3cf57e5a61b8218082ff0f42f998cd4618106faf
SankalpKadam <67373254+SankalpKadam@users.noreply.github.com>
2020-12-16 14:46:58 +0530

    Added scale and rotate for eyes

---
+++ animation.js
@@ -0,0 +1,348 @@
+function NormalToSad() {
+
+    //Normal To Sad
+    var mouth = KUTE.to('#mouth', { path: '#Sadmouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#lefteyebrow', { path: '#Sadleftbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#righteyebrow', { path: '#Sadrightbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#lefteye', { path: '#Sadlefteye' }, {
+            duration: 200,
+        })
+        .start();
+    var RightEye = KUTE.to('#righteye', { path: '#Sadrighteye' }, {
+            duration: 200,
+        })
+        .start();
+
+}
+
+function NormalToHappy() {
+    //Normal To Happy
+    var mouth = KUTE.to('#mouth', { path: '#Happymouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#lefteyebrow', { path: '#Happyleftbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#righteyebrow', { path: '#Happyrightbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#lefteye', { path: '#Happylefteye' }, {
+            duration: 200,
+        })
+        .start();
+    var righteye = KUTE.to('#righteye', { path: '#Happyrighteye' }, {
+            duration: 200,
+        })
+        .start();
+}
+
+function NormalToAngry() {
+    //Normal To Angry
+    var mouth = KUTE.to('#mouth', { path: '#Angrymouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#lefteyebrow', { path: '#LeftEyeBrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#righteyebrow', { path: '#RightEyeBrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#lefteye', { path: '#LeftEye' }, {
+            duration: 200,
+        })
+        .start();
+    var righteye = KUTE.to('#righteye', { path: '#RightEye' }, {
+            duration: 200,
+        })
+        .start();
+}
+
+function HappyToSad() {
+    //Happy To Sad
+    var mouth = KUTE.to('#Happymouth', { path: '#Sadmouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#Happyleftbrow', { path: '#Sadleftbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#Happyrightbrow', { path: '#Sadrightbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#Happylefteye', { path: '#Sadlefteye' }, {
+            duration: 200,
+        })
+        .start();
+    var RightEye = KUTE.to('#Happyrighteye', { path: '#Sadrighteye' }, {
+            duration: 200,
+        })
+        .start();
+
+}
+
+function HappyToAngry() {
+    //Happy To Angry
+    var mouth = KUTE.to('#Happymouth', { path: '#Angrymouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#Happyleftbrow', { path: '#LeftEyeBrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#Happyrightbrow', { path: '#RightEyeBrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#Happylefteye', { path: '#LeftEye' }, {
+            duration: 200,
+        })
+        .start();
+    var RightEye = KUTE.to('#Happyrighteye', { path: '#RightEye' }, {
+            duration: 200,
+        })
+        .start();
+}
+
+function HappyToNormal() {
+    //Happy To Normal
+    var mouth = KUTE.to('#Happymouth', { path: '#mouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#Happyleftbrow', { path: '#lefteyebrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#Happyrightbrow', { path: '#righteyebrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#Happylefteye', { path: '#lefteye' }, {
+            duration: 200,
+        })
+        .start();
+    var RightEye = KUTE.to('#Happyrighteye', { path: '#righteye' }, {
+            duration: 200,
+        })
+        .start();
+}
+
+function SadToHappy() {
+    //Sad To Happy
+    var mouth = KUTE.to('#Sadmouth', { path: '#Happymouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#Sadleftbrow', { path: '#Happyleftbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#Sadrightbrow', { path: '#Happyrightbrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#Sadlefteye', { path: '#Happylefteye' }, {
+            duration: 200,
+        })
+        .start();
+    var RightEye = KUTE.to('#Sadrighteye', { path: '#Happyrighteye' }, {
+            duration: 200,
+        })
+        .start();
+}
+
+function SadToAngry() {
+    //Sad To Angry
+    var mouth = KUTE.to('#Sadmouth', { path: '#Angrymouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#Sadleftbrow', { path: '#LeftEyeBrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#Sadrightbrow', { path: '#RightEyeBrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#Sadlefteye', { path: '#LeftEye' }, {
+            duration: 200,
+        })
+        .start();
+    var RightEye = KUTE.to('#Sadrighteye', { path: '#RightEye' }, {
+            duration: 200,
+        })
+        .start();
+}
+
+function SadToNormal() {
+    //Sad To Normal
+    var mouth = KUTE.to('#Sadmouth', { path: '#mouth' }, {
+            duration: 200,
+        })
+        .start();
+    var lefteyebrow = KUTE.to('#Sadleftbrow', { path: '#lefteyebrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var righteyebrow = KUTE.to('#Sadrightbrow', { path: '#righteyebrow' }, {
+            duration: 200,
+
+        })
+        .start();
+    var lefteye = KUTE.to('#Sadlefteye', { path: '#lefteye' }, {
+            duration: 200,
+        })
+        .start();
+    var RightEye = KUTE.to('#Sadrighteye', { path: '#righteye' }, {
+            duration: 200,
+        })
+        .start();
+}
+//Translate
+function MoveEyesUpDown() {
+    const TranslateYLeft_Up = KUTE.to("#lefteye", { translateY: -50 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    const TranslateYRight_Up = KUTE.to("#righteye", { translateY: -50 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    const TranslateYLeft_Down = KUTE.to("#lefteye", { translateY: 30 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    const TranslateYRight_Down = KUTE.to("#righteye", { translateY: 30 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    TranslateYLeft_Up.start();
+    TranslateYLeft_Up.chain(TranslateYLeft_Down);
+    TranslateYRight_Up.start();
+    TranslateYRight_Up.chain(TranslateYRight_Down);
+}
+
+function MoveEyesRightLeft() {
+    const TranslateXLeft_Left = KUTE.to("#lefteye", { translateX: -20 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    const TranslateXRight_Left = KUTE.to("#righteye", { translateX: -20 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    const TranslateXLeft_Right = KUTE.to("#lefteye", { translateX: 20 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    const TranslateXRight_Right = KUTE.to("#righteye", { translateX: 20 }, {
+        duration: 300,
+        yoyo: true,
+        repeat: 1
+    })
+    TranslateXLeft_Left.start();
+    TranslateXLeft_Left.chain(TranslateXLeft_Right);
+    TranslateXRight_Left.start();
+    TranslateXRight_Left.chain(TranslateXRight_Right);
+}
+
+function Rotate() {
+    const rotatelefteye = KUTE.to('#lefteye', { rotate: 360 }, {
+            duration: 1000,
+            yoyo: true,
+            repeat: 1
+        })
+        .start()
+    const rotaterighteye = KUTE.to('#righteye', { rotate: 360 }, {
+            duration: 1000,
+            yoyo: true,
+            repeat: 1
+        })
+        .start()
+}
+
+function Scale() {
+    const scalelefteye = KUTE.to('#lefteye', { scale: 1.2 }, {
+            duration: 700,
+            yoyo: true,
+            repeat: 1
+        })
+        .start()
+    const scalerighteye = KUTE.to('#righteye', { path: "#halfcloseeye" }, {
+            duration: 700,
+            yoyo: true,
+            repeat: 1
+        })
+        .start()
+    const scalelefteyebrow = KUTE.to('#lefteyebrow', { scale: 1.2 }, {
+            duration: 700,
+            yoyo: true,
+            repeat: 1
+        })
+        .start()
+    const scalerighteyebrow = KUTE.to('#righteyebrow', { translateY: 50 }, {
+            duration: 700,
+            yoyo: true,
+            repeat: 1
+        })
+        .start()
+}
+document.addEventListener('click', () => {
+
+    //MoveEyesUpDown();
+    //MoveEyesRightLeft();
+    //NormalToSad();
+    //NormalToHappy();
+    //NormalToAngry();
+    //HappyToSad();
+    //HappyToAngry();
+    //HappyToNormal();
+    //SadToHappy();
+    //SadToAngry();
+    //SadToNormal();
+    //Rotate();
+    //Scale();
+})
